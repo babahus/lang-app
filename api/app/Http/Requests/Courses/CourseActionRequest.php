aa3ff80f7b837e1785e0f3fb2b8c7267170e3521
Denis Kalinin <deniskalinin505@gmail.com>
2023-10-30 08:31:04 +0200

    For now, creator of course cannot subscribe to their own courses

--- api/app/Http/Requests/Courses/CourseActionRequest.php
+++ api/app/Http/Requests/Courses/CourseActionRequest.php
@@ -6,6 +6,8 @@ use App\DataTransfers\Courses\CourseActionDTO;
 use App\Http\Requests\BaseRequest;
 use App\Models\Course;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 final class CourseActionRequest extends BaseRequest
 {
@@ -13,6 +15,7 @@ final class CourseActionRequest extends BaseRequest
      * Determine if the user is authorized to make this request.
      *
      * @return bool
+     * @throws ValidationException
      */
     public function authorize()
     {
@@ -22,6 +25,12 @@ final class CourseActionRequest extends BaseRequest
 
         $course = Course::findOrFail($this->input('courseId'));
         $studentId = $this->input('studentId');
+
+        if ($studentId == $course->account_id){
+            $validator = Validator::make([], []);
+            $validator->errors()->add('courseId', 'A course creator cannot subscribe to their own course');
+            throw new ValidationException($validator);
+        }
 
         return Gate::allows('canManageEnrollment', [$course, $studentId]);
     }
